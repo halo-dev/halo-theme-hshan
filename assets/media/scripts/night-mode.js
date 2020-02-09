19f306607ca882072bd7c16846a4324c38bf887f
hshanx <ms915818993@163.com>
2020-02-09 13:38:55 +0800

    暗夜模式问题修复

--- assets/media/scripts/night-mode.js
+++ assets/media/scripts/night-mode.js
@@ -1,48 +1,57 @@
-const nightModeId = 'nightMode';
-const nightModeSmallId = 'nightModeSmall';
+const nightMode = 'nightMode';
+
 function autoNightMode() {
     var nightModes = $('.night-mode');
     var day = new Date();
     var D = day.getHours();
     var codeNight = $('#codeNight');
     var codeLight = $('#codeLight');
-    var isNightMode = getLocalStorage(nightModeId);
+    var isNightMode = getLocalStorage(nightMode);
     if (D <= 18 && D > 6) {
         // 白天
         if (isNightMode === true) {
             // 是暗黑模式
+            changeNightMode(codeNight, codeLight, nightModes);
             return;
         }
-        $(document.body).removeClass('night');
-        if (codeNight && codeLight) {
-            codeNight.attr("rel", "alternate stylesheet");
-            codeLight.attr("rel", "stylesheet");
-        }
-        for (let i = 0; i < nightModes.length; i++) {
-            let nightMode = $(nightModes[i]);
-            nightMode.addClass('fa-moon');
-            nightMode.removeClass('fa-lightbulb');
-        }
-        setLocalStorage(nightModeId, false)
+        changeLightMode(codeNight, codeLight, nightModes);
     } else {
         // 晚上
         if (isNightMode === false) {
             // 不是暗黑模式
+            changeLightMode(codeNight, codeLight, nightModes);
             return;
         }
-
-        $(document.body).addClass('night');
-        if (codeNight && codeLight) {
-            codeLight.attr("rel", "alternate stylesheet");
-            codeNight.attr("rel", "stylesheet");
-        }
-        for (let i = 0; i < nightModes.length; i++) {
-            let nightMode = $(nightModes[i]);
-            nightMode.addClass('fa-lightbulb');
-            nightMode.removeClass('fa-moon');
-        }
-        setLocalStorage(nightModeId, true)
+        changeNightMode(codeNight, codeLight, nightModes);
     }
+}
+
+function changeLightMode(codeNight, codeLight, nightModes) {
+    $(document.body).removeClass('night');
+    if (codeNight && codeLight) {
+        codeNight.attr("rel", "alternate stylesheet");
+        codeLight.attr("rel", "stylesheet");
+    }
+    for (let i = 0; i < nightModes.length; i++) {
+        let nightMode = $(nightModes[i]);
+        nightMode.addClass('fa-moon');
+        nightMode.removeClass('fa-lightbulb');
+    }
+    setLocalStorage(nightMode, false)
+}
+
+function changeNightMode(codeNight, codeLight, nightModes) {
+    $(document.body).addClass('night');
+    if (codeNight && codeLight) {
+        codeLight.attr("rel", "alternate stylesheet");
+        codeNight.attr("rel", "stylesheet");
+    }
+    for (let i = 0; i < nightModes.length; i++) {
+        let nightMode = $(nightModes[i]);
+        nightMode.addClass('fa-lightbulb');
+        nightMode.removeClass('fa-moon');
+    }
+    setLocalStorage(nightMode, true)
 }
 
 function nightModeFuc() {
@@ -50,7 +59,6 @@ function nightModeFuc() {
     if (!nightModes) {
         return;
     }
-    console.log(nightModes);
     for (let i = 0; i < nightModes.length; i++) {
         var nightMode = $(nightModes[i]);
         doFuncNightMode(nightMode);
@@ -59,36 +67,38 @@ function nightModeFuc() {
 }
 
 function doFuncNightMode(nightMode) {
+    var nightModeBtn = $('.night-mode');
     if ($(document.body).hasClass('night')) {
-        nightMode.addClass('fa-lightbulb');
-        nightMode.removeClass('fa-moon');
+        nightModeBtn.addClass('fa-lightbulb');
+        nightModeBtn.removeClass('fa-moon');
     } else {
-        nightMode.addClass('fa-moon');
-        nightMode.removeClass('fa-lightbulb');
+        nightModeBtn.addClass('fa-moon');
+        nightModeBtn.removeClass('fa-lightbulb');
     }
 
     var codeNight = $('#codeNight');
     var codeLight = $('#codeLight');
 
+
     nightMode.click(function (e) {
         if (nightMode.hasClass('fa-moon')) {
             $(document.body).addClass('night');
-            nightMode.addClass('fa-lightbulb');
-            nightMode.removeClass('fa-moon');
+            nightModeBtn.addClass('fa-lightbulb');
+            nightModeBtn.removeClass('fa-moon');
             if (codeNight && codeLight) {
                 codeLight.attr("rel", "alternate stylesheet");
                 codeNight.attr("rel", "stylesheet");
             }
-            setLocalStorage(nightModeId, true);
+            setLocalStorage(nightMode, true);
         } else if (nightMode.hasClass('fa-lightbulb')) {
             $(document.body).removeClass('night');
-            nightMode.addClass('fa-moon');
-            nightMode.removeClass('fa-lightbulb');
+            nightModeBtn.addClass('fa-moon');
+            nightModeBtn.removeClass('fa-lightbulb');
             if (codeNight && codeLight) {
                 codeNight.attr("rel", "alternate stylesheet");
                 codeLight.attr("rel", "stylesheet");
             }
-            setLocalStorage('nightMode', false);
+            setLocalStorage(nightMode, false);
         }
         $('#sidebar').removeClass('sidebar-show');
         $("#sidebarToggle").removeClass('menu-ctrl-on');
@@ -104,9 +114,9 @@ function setLocalStorage(key, value) {
     });
     try {
         localStorage.setItem(key, valueDate);
-    } catch(e) {
+    } catch (e) {
         // 兼容性写法
-        if(isQuotaExceeded(e)) {
+        if (isQuotaExceeded(e)) {
             console.log("Error: 本地存储超过限制");
             localStorage.clear();
         } else {
@@ -117,19 +127,19 @@ function setLocalStorage(key, value) {
 
 function isQuotaExceeded(e) {
     var quotaExceeded = false;
-    if(e) {
-        if(e.code) {
-            switch(e.code) {
+    if (e) {
+        if (e.code) {
+            switch (e.code) {
                 case 22:
                     quotaExceeded = true;
                     break;
                 case 1014: // Firefox
-                    if(e.name === 'NS_ERROR_DOM_QUOTA_REACHED') {
+                    if (e.name === 'NS_ERROR_DOM_QUOTA_REACHED') {
                         quotaExceeded = true;
                     }
                     break;
             }
-        } else if(e.number === -2147024882) { // IE8
+        } else if (e.number === -2147024882) { // IE8
             quotaExceeded = true;
         }
     }
@@ -138,12 +148,12 @@ function isQuotaExceeded(e) {
 
 function getLocalStorage(key) {
     var exp = 60 * 60 * 1000; // 一个小时的秒数
-    if(localStorage.getItem(key)) {
+    if (localStorage.getItem(key)) {
         var vals = localStorage.getItem(key); // 获取本地存储的值
         var dataObj = JSON.parse(vals); // 将字符串转换成JSON对象
         // 如果(当前时间 - 存储的元素在创建时候设置的时间) > 过期时间
         var isTimed = (new Date().getTime() - dataObj.timer) > exp;
-        if(isTimed) {
+        if (isTimed) {
             console.log("存储已过期");
             localStorage.removeItem(key);
             return null;
@@ -154,10 +164,6 @@ function getLocalStorage(key) {
     } else {
         return null;
     }
-}
-
-function changeNightMode() {
-
 }
 
 // 自动暗黑模式
