5c6ba21c269e8ce2a9594079700222cf981036b9
hshanx <ms915818993@163.com>
2020-02-23 00:38:33 +0800

    新增下拉菜单列表功能

--- assets/media/scripts/main.js
+++ assets/media/scripts/main.js
@@ -1,154 +1,163 @@
 /**
  * Main JS file for Subtle behaviours
  */
-
 // Responsive video embeds
-let videoEmbeds = [
+var videoEmbeds = [
 	'iframe[src*="youtube.com"]',
-	'iframe[src*="vimeo.com"]'
-];
-reframe(videoEmbeds.join(','));
+	'iframe[src*="vimeo.com"]',
+]
+reframe(videoEmbeds.join(','))
 
 // Smooth scroll to anchors
 var scroll = new SmoothScroll('[data-scroll]', {
 	speed: 300,
-	updateURL: false
-});
+	updateURL: false,
+})
 
 // Gallery adjustments
-var images = document.querySelectorAll('.kg-gallery-image img');
-images.forEach(function (image) {
-	var container = image.closest('.kg-gallery-image');
-	var width = image.attributes.width.value;
-	var height = image.attributes.height.value;
-	var ratio = width / height;
-	container.style.flex = ratio + ' 1 0%';
-});
-
-if (images.length) {
-	window.addEventListener("load", function() {
-		Lightense('.kg-gallery-image img', {
-			background: 'rgba(255,255,255,.9)',
-		});
-	}, false);
-}
+var images = document.querySelectorAll('.kg-gallery-image img')
+images.forEach(
+	function (image) {
+		var container = image.closest('.kg-gallery-image')
+		var width = image.attributes.width.value
+		var height = image.attributes.height.value
+		var ratio = width / height
+		container.style.flex = ratio + ' 1 0%'
+	})
+if (images.length)
+	window.addEventListener(
+		'load',
+		function () {
+			Lightense('.kg-gallery-image img', {
+				background: 'rgba(255,255,255,.9)',
+			})
+		},
+		false)
 
 // Sidebar toggle
-let sidebarToggle = document.querySelectorAll('.sidebar-toggle');
-if (sidebarToggle) {
-	for (let i = 0; i < sidebarToggle.length; i++) {
-		sidebarToggle[i].addEventListener('click', function (e) {
-			var menuCtrl = $("#sidebarToggle");
-			if (menuCtrl) {
-				menuCtrl.toggleClass('menu-ctrl-on');
-			}
-			var scrollMenu = $('#scrollMenu');
-			if (scrollMenu) {
-				scrollMenu.toggleClass('scroll-menu-show');
-			}
-			var siteHeader = $('#siteHeader');
-			if (siteHeader) {
-				siteHeader.toggleClass('site-header-scroll');
-			}
-			var sideBar = $('#sidebar');
-			if (sideBar) {
-				sideBar.toggleClass('sidebar-show');
-			}
-			e.preventDefault()
-			// 滚动条
-			$(document.body).toggleClass('cancel-scroll');
-		}, { passive: false });
-	}
-}
+var sidebarToggle = document.querySelectorAll('.sidebar-toggle')
+if (sidebarToggle)
+	for (var i = 0; i < sidebarToggle.length; i++)
+		sidebarToggle[i].addEventListener(
+			'click',
+			function (e) {
+				var menuCtrl = $('#sidebarToggle')
+				if (menuCtrl)
+					menuCtrl.toggleClass('menu-ctrl-on')
+				var scrollMenu = $('#scrollMenu')
+				if (scrollMenu)
+					scrollMenu.toggleClass('scroll-menu-show')
+				var siteHeader = $('#siteHeader')
+				if (siteHeader)
+					siteHeader.toggleClass('site-header-scroll')
+				var sideBar = $('#sidebar')
+				if (sideBar)
+					sideBar.toggleClass('sidebar-show')
+				e.preventDefault()
 
-$(function () {
-	function linksAddBlank() {
-		var links = $(".post-content a");
-		if (links) {
-			for (let i = 0; i < links.length; i++) {
-				$(links[i]).attr("target", "_blank");
+				// 滚动条
+				$(document.body).toggleClass('cancel-scroll')
+			},
+			{
+				passive: false,
+			})
+$(
+	function () {
+		function linksAddBlank() {
+			var links = $('.post-content a')
+			if (links)
+				for (var i = 0; i < links.length; i++)
+					$(links[i]).attr('target', '_blank')
+		}
 
+		var oldScrollTop
+
+		// 获取滚动条距离顶部位置
+
+		function getScrollTop() {
+			var scrollTop = 0
+			if (document.documentElement && document.documentElement.scrollTop)
+				scrollTop = document.documentElement.scrollTop
+			else
+				if (document.body)
+					scrollTop = document.body.scrollTop
+			return scrollTop
+		}
+
+		function scollHeader() {
+			window.addEventListener(
+				'scroll',
+				function () {
+					var siteHeader = $('#siteHeader')
+					var scrollMenu = $('#scrollMenu')
+					var sidebarToggle = $('#sidebarToggle')
+					var scrollTop = getScrollTop()
+					if (siteHeader && scrollTop > 0) {
+						siteHeader.addClass('site-header-scroll')
+						scrollMenu.addClass('scroll-menu-show')
+						sidebarToggle.hide()
+					} else {
+						siteHeader.removeClass('site-header-scroll')
+						scrollMenu.removeClass('scroll-menu-show')
+						sidebarToggle.show()
+					}
+					oldScrollTop = scrollTop
+				},
+				false)
+		}
+
+		function toggleSearch() {
+			// 搜索框
+			$('.js-toggle-search').on(
+				'click',
+				function () {
+					openSearchPanel()
+					$('#sidebar').removeClass('sidebar-show')
+					$('#sidebarToggle').removeClass('menu-ctrl-on')
+					$(document.body).removeClass('sidebar-opened')
+					$(document.body).removeClass('cancel-scroll')
+				})
+		}
+
+		// 当前菜单菜单高亮
+
+		function highlightMenu() {
+			var nav = document.getElementById('scrollMenu')
+			var links = nav.getElementsByTagName('a')
+			var currenturl = document.location.href
+			var last = 0
+			for (var i = 0; i < links.length; i++) {
+				var linkurl = links[i].getAttribute('href')
+				if (currenturl.indexOf(linkurl) !== -1)
+					last = i
+			}
+			$(links[last]).addClass('active-current')
+			var currentLinks = links[last]
+			if ($(currentLinks).parents('.sub-menu')) {
+				var subMenu = $(currentLinks).parents('.sub-menu')
+				$(subMenu[0]).siblings('a').addClass('active-current')
 			}
 		}
-	}
 
-	var oldScrollTop;
+		linksAddBlank()
 
-	//获取滚动条距离顶部位置
-	function getScrollTop() {
-		var scrollTop = 0;
-		if (document.documentElement && document.documentElement.scrollTop) {
-			scrollTop = document.documentElement.scrollTop;
-		} else if (document.body) {
-			scrollTop = document.body.scrollTop;
-		}
-		return scrollTop;
-	}
+		// 头部菜单滚动时间
+		scollHeader()
 
-	function scollHeader() {
-		window.addEventListener('scroll', function () {
-			var siteHeader = $("#siteHeader");
-			var scrollMenu = $("#scrollMenu");
-			var sidebarToggle = $("#sidebarToggle");
-			const scrollTop = getScrollTop();
-			if (siteHeader && scrollTop > 0) {
-				siteHeader.addClass('site-header-scroll');
-				scrollMenu.addClass('scroll-menu-show');
-				sidebarToggle.hide();
-			} else {
-				siteHeader.removeClass('site-header-scroll');
-				scrollMenu.removeClass('scroll-menu-show');
-				sidebarToggle.show();
-			}
-			oldScrollTop = scrollTop;
-		}, false);
-	}
+		toggleSearch()
 
-	function toggleSearch() { // 搜索框
-		$('.js-toggle-search').on('click', function () {
-			openSearchPanel();
-			$('#sidebar').removeClass('sidebar-show');
-			$("#sidebarToggle").removeClass('menu-ctrl-on');
-			$(document.body).removeClass('sidebar-opened');
-			$(document.body).removeClass('cancel-scroll');
-		});
-	}
-
-	// 当前菜单菜单高亮
-	function highlightMenu() {
-		var nav = document.getElementById("scrollMenu");
-		var links = nav.getElementsByTagName("a");
-		var currenturl = document.location.href;
-		var last = 0;
-		for (var i = 0; i < links.length; i++) {
-			var linkurl = links[i].getAttribute("href");
-			if (currenturl.indexOf(linkurl) !== -1) {
-				last = i;
-			}
-		}
-		$(links[last]).addClass('active-current');
-	}
-
-
-	linksAddBlank();
-
-	// 头部菜单滚动时间
-	scollHeader();
-
-	toggleSearch();
-	//当前目录菜单高亮
-	highlightMenu();
-});
+		// 当前目录菜单高亮
+		highlightMenu()
+	})
 
 // 删除日志中的空元素
-$(document).ready(function () {
-	var treeHoleBoxs = document.querySelectorAll('.tree-hole-box');
-	if (treeHoleBoxs) {
-		for (let i = 0; i < treeHoleBoxs.length; i++) {
-			var childrens = $(treeHoleBoxs[i]).children("p:empty");
-			for (let j = 0; j < childrens.length; j++) {
-				childrens[j].remove();
+$(document).ready(
+	function () {
+		var treeHoleBoxs = document.querySelectorAll('.tree-hole-box')
+		if (treeHoleBoxs)
+			for (var i = 0; i < treeHoleBoxs.length; i++) {
+				var childrens = $(treeHoleBoxs[i]).children('p:empty')
+				for (var j = 0; j < childrens.length; j++)
+					childrens[j].remove()
 			}
-		}
-	}
-});
+	})
--- module/header.ftl
+++ module/header.ftl
@@ -8,6 +8,16 @@
                         <#list menus?sort_by('priority') as menu>
                             <li class="menu-item home current-menu-item">
                                 <a href="${menu.url!}" target="${menu.target!}">${menu.name}</a>
+                                <#if menu.children?? && menu.children?size gt 0>
+                                    <ul class="nav-sub-menu">
+                                        <#list menu.children as child>
+                                            <li>
+                                                <a href="${child.url!}" target="${child.target!}"
+                                                   onfocus="this.blur();">${child.name}</a>
+                                            </li>
+                                        </#list>
+                                    </ul>
+                                </#if>
                             </li>
                         </#list>
                     </@menuTag>
@@ -31,25 +41,43 @@
 <header class="site-header" id="siteHeader">
     <div id="scrollMenu" class="scroll-menu header-inner">
         <div class="menu-items">
-            <#if settings.header_logoimg?? && settings.header_logoimg!=''>
-                <a href="${context!}" id="headerLogo" style="margin-right: 0; display: inline-block;height: 100%; line-height: 52px;" onfocus="this.blur();">
-                    <img src="${settings.header_logoimg}" alt="${options.blog_title!}"
-                            <#if settings.header_logo_height?? && settings.header_logo_height != ''>
-                                style=" height: ${settings.header_logo_height} "
-                            </#if>
-                    />
+            <ul>
+                <#if settings.header_logoimg?? && settings.header_logoimg!=''>
+                    <li>
+                        <a href="${context!}" id="headerLogo"
+                           style="margin-right: 0; display: inline-block;height: 100%; line-height: 52px;"
+                           onfocus="this.blur();">
+                            <img src="${settings.header_logoimg}" alt="${options.blog_title!}"
+                                    <#if settings.header_logo_height?? && settings.header_logo_height != ''>
+                                        style=" height: ${settings.header_logo_height} "
+                                    </#if>
+                            />
 
-                </a>
-            </#if>
-            <@menuTag method="tree">
-                <#list menus?sort_by('priority') as menu>
-                    <a href="${menu.url!}" target="${menu.target!}" onfocus="this.blur();" class="menu-scroll-item">${menu.name}</a>
-                </#list>
-            </@menuTag>
+                        </a>
+                    </li>
+                </#if>
+                <@menuTag method="tree">
+                    <#list menus?sort_by('priority') as menu>
+                        <li class="menu-scroll-item">
+                            <a href="${menu.url!}" target="${menu.target!}" onfocus="this.blur();">${menu.name}</a>
+                            <#if menu.children?? && menu.children?size gt 0>
+                                <ul class="sub-menu">
+                                    <#list menu.children as child>
+                                        <li>
+                                            <a href="${child.url!}" target="${child.target!}"
+                                               onfocus="this.blur();">${child.name}</a>
+                                        </li>
+                                    </#list>
+                                </ul>
+                            </#if>
+                        </li>
+                    </#list>
+                </@menuTag>
+            </ul>
         </div>
         <div class="other-ctl" id="headerCtrl">
             <#if settings.top_search!true>
-               <i class="fa fa-search js-toggle-search iconsearch" aria-hidden="true"></i>
+                <i class="fa fa-search js-toggle-search iconsearch" aria-hidden="true"></i>
             </#if>
             <#if settings.night_mode!true>
                 <i class="fa night-mode" id="nightMode" aria-hidden="true"></i>
