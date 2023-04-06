fe0dec191588e5e350d8db3b82d6cfe0bc061a19
fawen <1076697947@qq.com>
2023-04-06 15:30:26 +0800

    Convert the theme from freemarker to thymeleaf

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import "./styles/main.less"
-import "nprogress/nprogress.css"
-import NProgress from "nprogress"
-
-NProgress.start();
-document.addEventListener("DOMContentLoaded", function () {
-    NProgress.done()
-});
