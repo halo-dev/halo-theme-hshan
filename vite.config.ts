bbd9b57cb40a6979c22a0e6fbe552d615da649d8
Ryan Wang <i@ryanc.cc>
2023-04-18 11:28:00 +0800

    fix: bundle script
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import path from "path";
+import { fileURLToPath } from "url";
 
 export default defineConfig({
-    plugins: [],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, "src/main.ts"),
-            name: "main",
-            fileName: "main",
-            formats: ["umd"],
-        },
+  plugins: [],
+  build: {
+    outDir: fileURLToPath(new URL("./templates/assets/dist", import.meta.url)),
+    emptyOutDir: true,
+    lib: {
+      entry: path.resolve(__dirname, "src/main.ts"),
+      name: "main",
+      fileName: "main",
+      formats: ["umd"],
     },
+  },
 });
